e44f69cd0559118d1b104b8d3d3661f8cd3ed9f1
muhammed-gumus <mr.silver.mg@gmail.com>
2024-08-12 17:08:46 +0300

    feat: created new LoadingComponent

--- services/frontend/components/AllWordCard/index.tsx
+++ services/frontend/components/AllWordCard/index.tsx
@@ -36,54 +36,54 @@ const WordCard: React.FC = () => {
 
   if (loading) {
     return <LoadingComponent />;
+  } else {
+    return (
+      <div className="flex flex-col items-center justify-center mt-24 p-4 min-h-screen">
+        <div className="flex flex-wrap gap-8 justify-center w-full max-w-4xl">
+          {words.map((word) => (
+            <div
+              key={word.enWord}
+              className="bg-state-300 shadow-lg rounded-lg border border-gray-200 p-6 max-w-sm w-full"
+            >
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col mb-4">
+                  <h3 className="text-xl md:text-2xl font-semibold text-blue-600">
+                    English
+                  </h3>
+                  <p className="text-lg md:text-xl text-gray-800 mt-1">
+                    {word.enWord}
+                  </p>
+                </div>
+                <div className="flex flex-col mb-4">
+                  <h3 className="text-xl md:text-2xl font-semibold text-green-600">
+                    Turkish
+                  </h3>
+                  <p className="text-lg md:text-xl text-gray-800 mt-1">
+                    {word.trWord}
+                  </p>
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="text-xl md:text-2xl font-semibold text-purple-600">
+                    Example Sentence
+                  </h3>
+                  <p
+                    className="text-lg md:text-xl text-gray-800 mt-1"
+                    dangerouslySetInnerHTML={{
+                      __html: highlightWord(word.sentence, word.enWord),
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 
   // if (error) {
   //   return <ErrorComponent />;
   // }
-
-  return (
-    <div className="flex flex-col items-center justify-center mt-24 p-4 min-h-screen">
-      <div className="flex flex-wrap gap-8 justify-center w-full max-w-4xl">
-        {words.map((word) => (
-          <div
-            key={word.enWord}
-            className="bg-state-300 shadow-lg rounded-lg border border-gray-200 p-6 max-w-sm w-full"
-          >
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col mb-4">
-                <h3 className="text-xl md:text-2xl font-semibold text-blue-600">
-                  English
-                </h3>
-                <p className="text-lg md:text-xl text-gray-800 mt-1">
-                  {word.enWord}
-                </p>
-              </div>
-              <div className="flex flex-col mb-4">
-                <h3 className="text-xl md:text-2xl font-semibold text-green-600">
-                  Turkish
-                </h3>
-                <p className="text-lg md:text-xl text-gray-800 mt-1">
-                  {word.trWord}
-                </p>
-              </div>
-              <div className="flex flex-col">
-                <h3 className="text-xl md:text-2xl font-semibold text-purple-600">
-                  Example Sentence
-                </h3>
-                <p
-                  className="text-lg md:text-xl text-gray-800 mt-1"
-                  dangerouslySetInnerHTML={{
-                    __html: highlightWord(word.sentence, word.enWord),
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 };
 
 export default WordCard;
--- services/frontend/components/LoadingComponent/index.tsx
+++ services/frontend/components/LoadingComponent/index.tsx
@@ -3,12 +3,88 @@
 export default function Navbar() {
   return (
     <div
-      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+      aria-label="Loading..."
       role="status"
+      className="min-h-screen w-full flex items-center opacity-70 justify-center space-x-2"
     >
-      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-        Loading...
-      </span>
+      <svg
+        className="h-20 w-20 animate-spin stroke-black"
+        viewBox="0 0 256 256"
+      >
+        <line
+          x1="128"
+          y1="32"
+          x2="128"
+          y2="64"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="195.9"
+          y1="60.1"
+          x2="173.3"
+          y2="82.7"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="224"
+          y1="128"
+          x2="192"
+          y2="128"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="195.9"
+          y1="195.9"
+          x2="173.3"
+          y2="173.3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="128"
+          y1="224"
+          x2="128"
+          y2="192"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="60.1"
+          y1="195.9"
+          x2="82.7"
+          y2="173.3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="32"
+          y1="128"
+          x2="64"
+          y2="128"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+        <line
+          x1="60.1"
+          y1="60.1"
+          x2="82.7"
+          y2="82.7"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        ></line>
+      </svg>
+      <span className="text-4xl font-medium text-black">Loading...</span>
     </div>
   );
 }
